3d3068e579d19e0cc79320dfddd51262367b7807
Mikhail Ustinov <mikhailu359@gmail.com>
2023-07-07 16:17:25 +0300

    upgrade CheckJsonTest

--- src/test/java/com/justforge359/CheckJsonTest.java
+++ src/test/java/com/justforge359/CheckJsonTest.java
@@ -2,25 +2,27 @@ package com.justforge359;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.justforge359.dto.PesDto;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CheckJsonTest {
     ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     void checkJsonTest() throws IOException {
         File file = new File("src/test/resources/pes.json");
         PesDto dto = objectMapper.readValue(file, PesDto.class);
 
-        Assertions.assertEquals("Pirozhok", dto.getName());
-        Assertions.assertEquals("Samoyed", dto.getBreed());
-        Assertions.assertEquals("white", dto.getDogCharacteristics().getColor());
-        Assertions.assertEquals("black", dto.getDogCharacteristics().getNose());
-        Assertions.assertEquals(true, dto.getDogCharacteristics().isTailWiggly());
-        Assertions.assertEquals("cunning", dto.getDogCharacteristics().getEyes());
-        Assertions.assertEquals(5, dto.getAge());
+        assertEquals("Pirozhok", dto.getName());
+        assertEquals("Samoyed", dto.getBreed());
+        assertEquals("white", dto.getDogCharacteristics().getColor());
+        assertEquals("black", dto.getDogCharacteristics().getNose());
+        assertEquals(true, dto.getDogCharacteristics().isTailWiggly());
+        assertEquals("cunning", dto.getDogCharacteristics().getEyes());
+        assertEquals(5, dto.getAge());
     }
 }
